44f61cf3b3cf97ccb4e1cfb633023b75e1e4bba1
Carola Lilienthal <cl@wps.de>
2021-01-07 19:11:03 +0100

    import corrected

--- Bank Step3/src/tests/CreditServiceTest.java
+++ Bank Step3/src/tests/CreditServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import accounting.AccountManagementService;
 import credit.Credit;
 import credit.CreditAccount;
 import credit.CreditCustomer;
